dca5a125a573ef00612f2d44bc6e0f2cb748c981
Jeremy Myers <echelonical@gmail.com>
2023-03-07 11:11:43 -0600

    inital commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+/* 
+  Goals of project:
+    1. Header for each exhibit of the zoo
+    2. Under header list all animals in that exhibit
+    3. As an aside on the right, list all concessions
+    4. Main header at top with "Welcome to Spring Hill Zoo"
+*/
+
+
+/* 
+  Module Responsibility:
+    1. Update the DOM with *all* of the HTML
+    2. Defer creation of specific sections to other modules
+*/
+
